01b75834a508e54ea6e2525048778bd654310f38
Sean <shuoseanfeng@gmail.com>
2016-12-06 00:21:04 -0500

    Added some messages, just start

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 The main program for CSC384 project
 '''
 
+WELCOME_MESSAGE = "Thanks for using IMeal"
+ASK_CALORIE = "Enter how much calories do you want to have: "
+ASK_NUTATION_PER_MEAL = "Enter how much nutrition do you want to have per meal: "
+BUDGET = "Enter a budget limitation: "
+SPECIAL_CONSIDERATION = "Select a special request: "
 def main():
     """
     The main function
@@ -14,8 +19,16 @@ def main():
     #   2) Number of meals to be generated
     #   3）Nutrition amount per meal/day/plan
     #   4) Budget
-    #   5) Special Considerations such as "no two idential meas in a day" or
+    #   5) Special Considerations such as "no two idential meals in a day" or
     # "vegetarian" and etc
+    print(WELCOME_MESSAGE)
+    while True:
+        try:
+            calories = int(input(ASK_CALORIE))
+        except ValueError:
+            print("Sorry please enter a number or None")
+
+
     pass
 
 
